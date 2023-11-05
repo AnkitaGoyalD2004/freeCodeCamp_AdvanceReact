ac1e0d7a244f58a05a200216eb63e23ede6dd873
AnkitaGoyalD2004 <goyalankita0325@gmail.com>
2023-11-05 17:16:03 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Starter from "./tutorial/01-useState/starter/04-useState-object";
+import Starter from "./tutorial/01-useState/starter/05-useState-gotcha";
 // import final from "./tutorial/01-useState/final/03-useState-array";
 function App() {
   return (
