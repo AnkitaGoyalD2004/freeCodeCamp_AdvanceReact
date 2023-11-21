c00bcb4185e3d3f53132e1af4ecbd88c99dfb983
AnkitaGoyalD2004 <goyalankita0325@gmail.com>
2023-11-21 19:24:10 +0530

    update

--- src/tutorial/09-context-api/starter/Navbar.jsx
+++ src/tutorial/09-context-api/starter/Navbar.jsx
@@ -1,8 +1,14 @@
+import { useState } from "react";
+import NavLinks from "./NavLinks";
 const Navbar = () => {
+  const [user, setUser] = useState({ name: "Abhita" });
+  const logout = () => {
+    setUser(null);
+  };
   return (
     <nav className="navbar">
       <h5>CONTEXT API</h5>
-      <NavLinks />
+      <NavLinks user={user} logout={logout} />
     </nav>
   );
 };
