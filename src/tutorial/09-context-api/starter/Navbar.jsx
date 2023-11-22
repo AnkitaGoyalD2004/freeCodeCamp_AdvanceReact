309db2b46a22d5d22c9e3333a2bc8c5e1d8da3ba
AnkitaGoyalD2004 <goyalankita0325@gmail.com>
2023-11-22 07:16:38 +0530

    update

--- src/tutorial/09-context-api/starter/Navbar.jsx
+++ src/tutorial/09-context-api/starter/Navbar.jsx
@@ -1,15 +1,24 @@
-import { useState } from "react";
+import { useState , createContext } from "react";
 import NavLinks from "./NavLinks";
+
+export const NavbarContext = createContext();
+
+// custom hook
+export const useAppContext = () => 
+
 const Navbar = () => {
+  React.createContext();
   const [user, setUser] = useState({ name: "Abhita" });
   const logout = () => {
     setUser(null);
   };
   return (
-    <nav className="navbar">
-      <h5>CONTEXT API</h5>
-      <NavLinks user={user} logout={logout} />
-    </nav>
+    <NavbarContext.Provider value={{user,logout}}>
+      <nav className="navbar">
+        <h5>CONTEXT API</h5>
+        <NavLinks user={user} logout={logout} />
+      </nav>
+    </NavbarContext.Provider>
   );
 };
 
--- src/tutorial/09-context-api/starter/UserContainer.jsx
+++ src/tutorial/09-context-api/starter/UserContainer.jsx
@@ -1,10 +1,24 @@
-const UserContainer = ({ user, logout }) => {
+import { useContext } from "react";
+import { NavbarContext, useAppContext } from "./Navbar";
+
+const UserContainer = () => {
+  const { user, logout } = useAppContext;
+  //  console.log(value);
+
+  return "helloWorld";
   return (
     <div className="user-container">
-      <p>Hello there , {user.name}</p>
-      <button className="btn" onClick={logout}>
-        logout
-      </button>
+      {user ? (
+        <>
+          <p>Hello there , {user?.name?.toUpperCase()}</p>
+
+          <button className="btn" onClick={logout}>
+            logout
+          </button>
+        </>
+      ) : (
+        <p>Please Login</p>
+      )}
     </div>
   );
 };
